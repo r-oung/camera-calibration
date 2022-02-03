e1216022fabe3af55adde9bfbba38b4ed697b066
R. Oung <44389573+r-oung@users.noreply.github.com>
2022-02-03 13:31:47 +0900

    Adds license notice

--- calibrate.py
+++ calibrate.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 """Camera Calibration
 
+Calculates calibration parameters from a set of images.
+
+Copyright (C) 2021-2022  Raymond Oung
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import cv2
 import numpy as np
@@ -72,6 +88,16 @@ for filename in filename_list:
 
 if ARGS.show:
     cv2.destroyAllWindows()
+
+# Print license notice
+print("""
+dancebots-server  Copyright (C) 2021-2022  Raymond Oung
+This program comes with ABSOLUTELY NO WARRANTY.
+This is free software, and you are welcome to redistribute it
+under certain conditions. Refer to the license notice at the
+top of the script.
+
+""")
 
 # Calibrate camera
 print("Calculating calibration parameters...")
